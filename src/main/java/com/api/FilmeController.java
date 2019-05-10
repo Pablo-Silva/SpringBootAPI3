69da76a7c92f86e8061b4e2189451f9b2e2d83e7
Thaina Mariani <marianithaina@gmail.com>
2019-05-10 02:14:24 -0300

    Initial commit

--- src/main/java/com/api/FilmeController.java
+++ src/main/java/com/api/FilmeController.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @CrossOrigin
+@RestController
 public class FilmeController {
 
 	@Autowired
--- src/main/java/com/api/FilmeRepository.java
+++ src/main/java/com/api/FilmeRepository.java
@@ -3,6 +3,8 @@ package com.api;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {}
